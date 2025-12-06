59f471fbffc98d56e4581ae1748e0e4b5ea44318
Komori <121684959+Komorif@users.noreply.github.com>
2025-12-06 22:05:34 +0300

    Backlinks

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -30,20 +30,6 @@ export default ((opts?: Partial<BacklinksOptions>) => {
     }
     return (
       <div class={classNames(displayClass, "backlinks")}>
-        <h3>{i18n(cfg.locale).components.backlinks.title}</h3>
-        <OverflowList>
-          {backlinkFiles.length > 0 ? (
-            backlinkFiles.map((f) => (
-              <li>
-                <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
-                  {f.frontmatter?.title}
-                </a>
-              </li>
-            ))
-          ) : (
-            <li>{i18n(cfg.locale).components.backlinks.noBacklinksFound}</li>
-          )}
-        </OverflowList>
       </div>
     )
   }
